4cea2c332062a8f59b6320b332271d2b25995d9b
greenmoon55 <greenmoon55@gmail.com>
2014-03-22 19:27:23 +0800

    code cleanup

--- src/fi/uef/cs/SimilariyMetric.java
+++ src/fi/uef/cs/SimilariyMetric.java
@@ -39,89 +39,24 @@ public class SimilariyMetric {
 			
 		} 
 		
-		String similariString = getSimilarityNew(word1, word2, method, type);
+		String similariString = getSimilarity(word1, word2, method, type);
 		System.out.println("The similarity of "+word1 +" and " +word2+" is: "+similariString);
 		
-		
-
-//		String word1 = "bike";
-//		String word2 = "article";
-//		String type= "n";
-
-		// get similarity
-		
-		
-
-		
-
-
-//		System.out.println("The WordSimilarity of " + "cat" + " and " + "dog"
-//				+ " is: " + wordsimilarityValue);
-		
-		//find the gloss in the wordnet
-		/*
-		HashMap<Integer, String> wordglossHashMap = new HashMap<Integer, String>();
-		try {
-			wordglossHashMap = WordnetSimilarity.getWordGloss(word1, "n");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Set set = wordglossHashMap.entrySet();
-		Map.Entry<Integer, String> entry = null;
-		Iterator it = set.iterator();
-
-		System.out.println(word1 + " has the following sences: ");
-		while (it.hasNext()) {
-			entry = (Map.Entry<Integer, String>) it.next();
-
-			System.out.println(entry.getKey() + "  " + entry.getValue());
-
-		}*/
-
 	}
 	
 	public static String getSimilarity(String word1, String word2, int method, String type){
-		double similarityValue = 0;
-		try {
-			//1.Jiang 2.Wu 3.Lin 4.Path 5.Levenshtein
-			if (method ==1 || method ==2 || method ==3 || method==4) {
-				WordnetSimilarity wSimilarity=new WordnetSimilarity();
-				similarityValue = wSimilarity.getWordnetSimilarity(word1, word2, type, method);
-				if (similarityValue>1){
-					similarityValue=1;
-				}else if (similarityValue<0){
-					similarityValue=0;
-				}
-
-			} else if (method ==5 || method ==6 || method ==7 || method==8) {
-				StringSimilarity stringSimilarity= new StringSimilarity();
-				similarityValue = stringSimilarity.getStringSimilarity(word1, word2, type, method);
-		
-			}else{
-				System.out.println("Sorry, no this method!!!");
-				similarityValue=-1;
-			}
-		
-//wordsimilarityValue = wSimilarity.getWordnetSimilarity("cat", "dog", "n");
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-		java.text.DecimalFormat   df=new   java.text.DecimalFormat("#0.00"); 
-		String similarityValue1 = df.format(similarityValue);		
-		return similarityValue1;
+		return getSimilarity(word1, word2, method, type, false);
 	}
 	
-	public static String getSimilarityNew(String word1, String word2,
-			int method, String type) {
+	public static String getSimilarity(String word1, String word2,
+			int method, String type, boolean firstSenseOnly) {
 		double similarityValue = 0;
 		try {
 			// 1.Jiang 2.Wu 3.Lin 4.Path 5.Levenshtein
 			if (method == 1 || method == 2 || method == 3 || method == 4) {
 				WordnetSimilarity wSimilarity = new WordnetSimilarity();
 				similarityValue = wSimilarity.getWordnetSimilarity(word1,
-						word2, type, method);
+						word2, type, method, firstSenseOnly);
 				if (similarityValue > 1) {
 					similarityValue = 1;
 				} else if (similarityValue < 0) {
--- src/fi/uef/cs/WordnetSimilarity.java
+++ src/fi/uef/cs/WordnetSimilarity.java
@@ -20,9 +20,6 @@ public class WordnetSimilarity {
 	static final String dir = "D:/Software/WordNet";
 	static final String version = "2.1";
 
-	File directory = new File("");
-	// String dir= directory.getAbsolutePath()+"/WordNet";
-
 	JWS ws = new JWS(dir, "2.1");
 	WuAndPalmer wup = ws.getWuAndPalmer();
 	JiangAndConrath jiang = ws.getJiangAndConrath();
@@ -37,141 +34,50 @@ public class WordnetSimilarity {
 		Lin lin = ws.getLin();
 		// 4 Path
 		Path path = ws.getPath();
+		double similarityValue = -1;
 		if (firstSenseOnly) {
 			if (method == 1) {
-				double similarityValue = jiang.jcn(word1, 1, word2, 1,
-						type);
-				return similarityValue;
+				similarityValue = jiang.jcn(word1, 1, word2, 1, type);
 			} else if (method == 2) {
-				double similarityValue = lin.lin(word1, 1, word2, 1, type);
-				return similarityValue;
+				similarityValue = wup.wup(word1, 1, word2, 1, type);
 			} else if (method == 3) {
-				double similarityValue = path.path(word1, 1, word2, 1, type);
-				return similarityValue;
+				similarityValue = lin.lin(word1, 1, word2, 1, type);
 			} else if (method == 4) {
-				double similarityValue = wup.wup(word1, 1, word2, 1, type);
-				return similarityValue;
-			} else {
-				return 0;
+				similarityValue = path.path(word1, 1, word2, 1, type);
 			}
 		} else {
-			double similarityValue = 0;
 			if (method == 1) {
 				similarityValue = jiang.max(word1, word2, type);
-				return similarityValue;
 			} else if (method == 2) {
 				similarityValue = wu.max(word1, word2, type);
-				return similarityValue;
 			} else if (method == 3) {
 				similarityValue = lin.max(word1, word2, type);
-				return similarityValue;
 			} else if (method == 4) {
 				similarityValue = path.max(word1, word2, type);
-				return similarityValue;
 			} else {
 				similarityValue = -1;
-				return similarityValue;
 			}
 		}
+		return similarityValue;
 	}
 
-	/*
-	 * get the similarity by just comparing the all the meaning of two words by
-	 * five different measure 1. JiangAndConrath 2. Lin 3. Path 4. WuAndPalmer
-	 */
-	public double getWordnetSimilarityByMeasure(String word1, String word2,
-			String type, String measure) {
-
-		if (measure.trim().equalsIgnoreCase("1")) {
-			JiangAndConrath jiangAndConrath = ws.getJiangAndConrath();
-			double similarityValue = jiangAndConrath.jcn(word1, 1, word2, 1,
-					type);
-			return similarityValue;
-		} else if (measure.trim().equalsIgnoreCase("2")) {
-			Lin lin = ws.getLin();
-			double similarityValue = lin.lin(word1, 1, word2, 1, type);
-			return similarityValue;
-		} else if (measure.trim().equalsIgnoreCase("3")) {
-			Path path = ws.getPath();
-			double similarityValue = path.path(word1, 1, word2, 1, type);
-			return similarityValue;
-		} else if (measure.trim().equalsIgnoreCase("4")) {
-			double similarityValue = wup.wup(word1, 1, word2, 1, type);
-			return similarityValue;
-		} else {
-			return 0;
-		}
-	}
-
-	/*
-	 * get the similarity by just comparing the first the meaning of two words
-	 * by five different measure 1. JiangAndConrath 2. Lin 3. Path 4.
-	 * WuAndPalmer
-	 */
-	public double getWordnetSimilarityAllByMeasure(String word1, String word2,
-			String type, String measure) {
-
-		if (measure.trim().equalsIgnoreCase("1")) {
-			JiangAndConrath jiangAndConrath = ws.getJiangAndConrath();
-			double similarityValue = jiangAndConrath.max(word1, word2, type);
-			return similarityValue;
-		} else if (measure.trim().equalsIgnoreCase("2")) {
-			Lin lin = ws.getLin();
-			double similarityValue = lin.max(word1, word2, type);
-			return similarityValue;
-		} else if (measure.trim().equalsIgnoreCase("3")) {
-			Path path = ws.getPath();
-			double similarityValue = path.max(word1, word2, type);
-			return similarityValue;
-
-		} else if (measure.trim().equalsIgnoreCase("4")) {
-
-			double similarityValue = wup.max(word1, word2, type);
-			return similarityValue;
-		} else {
-			return 0;
-		}
-	}
-
-	public <E> double getWordDistanceAll(String word1, String word2, String type) {
-
-		// JiangAndConrath wup=ws.getJiangAndConrath();
-
-		// TreeMap<String, Double> scores1 = jcn.jcn("apple", "banana", "n"); //
-		// all
-
-		// double similarityValue = wup.max(word1, word2, type);
-		double wordsimilarityValue = wup.max(word1.toString(),
-				word2.toString(), type);
-		double worddistanceValue = 1 - wordsimilarityValue;
-		java.text.DecimalFormat df = new java.text.DecimalFormat("#0.00");
-		double distacneValue = Double.parseDouble(df
-				.format(worddistanceValue * 100));
-		return distacneValue;
-	}
 
 	public static HashMap<Integer, String> getWordGloss(String keyword,
 			String type) throws IOException {
 		HashMap<Integer, String> glossMap = new HashMap<Integer, String>();
 		POS postype = null;
 
-		String wordtype = null;
 		if (type.equalsIgnoreCase("n")) {
 			postype = POS.NOUN;
-			wordtype = "NOUN";
 		} else if (type.equalsIgnoreCase("v")) {
 			postype = POS.VERB;
-			wordtype = "VERB";
 		} else if (type.equalsIgnoreCase("a")) {
 			postype = POS.ADJECTIVE;
-			wordtype = "ADJECTIVE";
 		} else if (type.equalsIgnoreCase("r")) {
 			postype = POS.ADVERB;
-			wordtype = "ADVERB";
 		}
 
 		String path = dir + File.separator + version + File.separator + "dict";
-		// String path = "D:/WordNet-3.0" + File.separator + "dict";
 
 		URL url = new URL("file", null, path);
 
@@ -190,7 +96,6 @@ public class WordnetSimilarity {
 			} catch (ArrayIndexOutOfBoundsException e) {
 				break;
 			}
-
 		}
 		return glossMap;
 	}
