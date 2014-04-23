a28f446b2bf4e93eb055f8efe719357cc86df383
greenmoon55 <greenmoon55@gmail.com>
2014-04-23 22:18:53 +0800

    test generics method HierachicalClustering

--- src/fi/uef/cs/ShortTextSimilarity.java
+++ src/fi/uef/cs/ShortTextSimilarity.java
@@ -1,12 +1,10 @@
 package fi.uef.cs;
 
-import java.io.IOException;
 import java.util.*;
 
 import com.aliasi.cluster.Dendrogram;
 import com.aliasi.cluster.LeafDendrogram;
 import com.aliasi.cluster.LinkDendrogram;
-import com.aliasi.cluster.AbstractHierarchicalClusterer;
 import com.aliasi.util.Scored;
 import com.aliasi.util.ScoredObject;
 
@@ -33,7 +31,7 @@ public class ShortTextSimilarity {
 		return maxSimilarity;
 	}
 
-	public double getSimilarity(List<String> wordArray1, List<String> wordArray2,
+	private double getSimilarity(List<String> wordArray1, List<String> wordArray2,
 			SimilarityMetric.Method method, String type, boolean firstSenseOnly) {
 		if (wordArray1.size() < wordArray2.size()) {
 			List<String> temp = wordArray1;
@@ -72,25 +70,18 @@ public class ShortTextSimilarity {
                 + ") ";
         }
     }
-	
-	public List<HashMap<List<List<String>>, Double>> HierachicalClustering(ArrayList<List<String>> data, SimilarityMetric.Method method) {
-		double[][] similarityMatrix = new double[data.size()][data.size()];
-		for (int i = 0; i < data.size(); i++) {
-			for (int j = i + 1; j < data.size(); j++) {
-				similarityMatrix[i][j] = similarityMatrix[j][i] = getSimilarity(data.get(i), data.get(j), method, "n", false);
-			}
-		}
 		
+	public <T> Dendrogram<T> HierachicalClustering(ArrayList<T> data, SimilarityMetric.Method method, double[][] similarityMatrix) {
 		// 数组 elements 变成 LeafDendrogram数组 array
-		LeafDendrogram<List<String>>[] leafs = (LeafDendrogram<List<String>>[]) new LeafDendrogram[data.size()];
+		LeafDendrogram<T>[] leafs = (LeafDendrogram<T>[]) new LeafDendrogram[data.size()];
 		for (int i = 0; i < data.size(); i++) {
-			leafs[i] = new LeafDendrogram<List<String>>(data.get(i));
+			leafs[i] = new LeafDendrogram<T>(data.get(i));
 		}
 
 		// LeafDendrogram数组 变成 set 容器 装满系统树形的容器
-		Set<Dendrogram<List<String>>> clusters = new HashSet<Dendrogram<List<String>>>(
+		Set<Dendrogram<T>> clusters = new HashSet<Dendrogram<T>>(
 				data.size());
-		for (Dendrogram<List<String>> dendrogram : leafs) {
+		for (Dendrogram<T> dendrogram : leafs) {
 			clusters.add(dendrogram);
 		}
 		
@@ -100,7 +91,7 @@ public class ShortTextSimilarity {
 		 */
 
 		// pairscore 有一个构造方法，二个Dendrogram， 加一个score
-		ArrayList<PairScore<List<String>>> pairScoreList = new ArrayList<PairScore<List<String>>>();
+		ArrayList<PairScore<T>> pairScoreList = new ArrayList<PairScore<T>>();
 		double maxDistance = Double.MAX_VALUE;
 		
 
@@ -113,7 +104,7 @@ public class ShortTextSimilarity {
 //							+ distanceIJ);
 				if (distanceIJ > maxDistance)
 					continue;
-				pairScoreList.add(new PairScore<List<String>>(leafs[i], leafs[j],
+				pairScoreList.add(new PairScore<T>(leafs[i], leafs[j],
 						distanceIJ));
 			}
 		}
@@ -125,8 +116,7 @@ public class ShortTextSimilarity {
 		 */
 
 		// pairscore 从小到大排列ps(car,jeep:0.0) ps(cat,woman:4.76)
-		PairScore<List<String>>[] pairScores = (PairScore<List<String>>[]) new PairScore[pairScoreList
-				.size()];
+		PairScore<T>[] pairScores = (PairScore<T>[]) new PairScore[pairScoreList.size()];
 		pairScoreList.toArray(pairScores);
 		Arrays.sort(pairScores, ScoredObject.comparator()); // increasing order
 															// of distance
@@ -139,12 +129,12 @@ public class ShortTextSimilarity {
 		 */
 //			System.out.println("----------------------------");
 		for (int i = 0; i < pairScores.length && clusters.size() > 1; ++i) {
-			PairScore<List<String>> ps = pairScores[i];
+			PairScore<T> ps = pairScores[i];
 			if (ps.score() > Double.MAX_VALUE)
 				break;
 			// ps.mDendrogram1 是组成ps 里的第一个LeafDendrogram dereference 返回它的parent
-			Dendrogram<List<String>> d1 = ps.mDendrogram1.dereference();
-			Dendrogram<List<String>> d2 = ps.mDendrogram2.dereference();
+			Dendrogram<T> d1 = ps.mDendrogram1.dereference();
+			Dendrogram<T> d2 = ps.mDendrogram2.dereference();
 
 //				System.out.println(ps.mDendrogram1 + " d1 dereference: " + d1);
 //				System.out.println(ps.mDendrogram2 + " d2 dereference: " + d2);
@@ -157,36 +147,38 @@ public class ShortTextSimilarity {
 			clusters.remove(d2);
 			// 把d1， d2 的parent 设置成新生成的 有分支的系统树形 dlink， pairScores[i].mScore
 			// 是distance数字
-			LinkDendrogram<List<String>> dLink = new LinkDendrogram<List<String>>(d1, d2,
+			LinkDendrogram<T> dLink = new LinkDendrogram<T>(d1, d2,
 					pairScores[i].mScore);
 			clusters.add(dLink);
 		}
 
 		// link up remaining unlinked dendros at +infinity distance
-		Iterator<Dendrogram<List<String>>> it = clusters.iterator();
-		Dendrogram<List<String>> dendro = it.next(); // skip first -
+		Iterator<Dendrogram<T>> it = clusters.iterator();
+		Dendrogram<T> dendro = it.next(); // skip first -
 												// self,被排好的系统树形就一个，如果还有，连好的和没连号的连
 		while (it.hasNext()) {
-			dendro = new LinkDendrogram<List<String>>(dendro, it.next(),
+			dendro = new LinkDendrogram<T>(dendro, it.next(),
 					Double.POSITIVE_INFINITY);
 		}
 		
-		for (int i = 0; i < 88; i++) {
-			Set<Set<List<String>>> partitions = dendro.partitionK(i + 1);
-			System.out.println(" ");
-			System.out.println(i + " clusters");
-			for (Set<List<String>> stringListSet: partitions) {
-				System.out.println("set");
-				for (List<String> stringList: stringListSet) {
-					for (String string: stringList) {
-						System.out.print(string + ",");
-					}
-					System.out.print(" ");
-				}
-				System.out.println(" ");
-			}
-		}
-		return null;
+		return dendro;
+		
+//		for (int i = 0; i < 88; i++) {
+//			Set<Set<List<String>>> partitions = dendro.partitionK(i + 1);
+//			System.out.println(" ");
+//			System.out.println(i + " clusters");
+//			for (Set<List<String>> stringListSet: partitions) {
+//				System.out.println("set");
+//				for (List<String> stringList: stringListSet) {
+//					for (String string: stringList) {
+//						System.out.print(string + ",");
+//					}
+//					System.out.print(" ");
+//				}
+//				System.out.println(" ");
+//			}
+//		}
+//		return null;
 	}
 	
 }
