236a9ae71c8772cfb3901f311cf693208e010fa3
greenmoon55 <greenmoon55@gmail.com>
2014-05-19 15:37:22 +0800

    new ssw ssb methods for spectral clustering

--- src/fi/uef/cs/ShortTextSimilarity.java
+++ src/fi/uef/cs/ShortTextSimilarity.java
@@ -101,67 +101,153 @@ public class ShortTextSimilarity {
 		return similarityMatrix;
 	}
 	
-	public double getSSWForString(Set<String> inputSet, Set<Set<String>> slKClustering, HashMap<UnorderedPair<String>, Double> similarityMap) {
-		List<Double> ssw = new ArrayList<Double>();
-		for (int k = 1; k <= dendrogram.size(); ++k) {
-			Set<Set<String>> slKClustering = dendrogram.partitionK(k);
-			int normalizationFactor = 0;
-			double maxdistanceAll = 0;
-			int numSingleObject = 0;
-			for (Set<String> set : slKClustering) {
-				int setSize = set.size();
-				double maxdistance = 0;
-				double distanceIJ;
-				String[] arrayStrings = setToArray(set);
-				if (setSize > 1) {
-					// 得到所有的pair 的distance， 放到pairscore 的list 里面
-					for (int i = 0; i < arrayStrings.length; i++) {
-						String sI = arrayStrings[i];
-
-						for (int j = i + 1; j < arrayStrings.length; j++) {
-							String sJ = arrayStrings[j];
-							// double distanceIJ=
-							// wordnetSimilarity.getWordnetSimilarityAll(sI,
-							// sJ,type);
-							double similarityValue = similarityMap.get(new UnorderedPair<String>(sI, sJ));
-							distanceIJ = 1 - similarityValue;
-//							System.out.println(sI + "  " + sJ
-//									+ " the distance is: " + distanceIJ);
-//							totaldistance += distanceIJ;
-							if (distanceIJ > maxdistance) {
-								maxdistance = distanceIJ;
-							}
-							
-						}
-					}
-					normalizationFactor += 1;
-//					totaldistance += totaldistance_temp;
-//					totaldistance_temp /= normalizationFactor;
-//					totaldistance += totaldistance_temp;
-				} else {
-					numSingleObject += 1/arrayStrings.length;
-				}
-				
-				if (maxdistance > maxdistanceAll)
-					maxdistanceAll = maxdistance;				
-			}
-			
-//			if ((normalized.trim().equalsIgnoreCase("y")) && (normalizationFactor != 0) )
-//				totaldistance /= normalizationFactor;
-			// System.out.println(k + "  " + slKClustering);
-			ssw.add(maxdistanceAll + numSingleObject);
-			//System.out.println(maxdistanceAll);
-//			System.out.println(k + "  " + slKClustering + ". ssw is: "
-//					+ totaldistance);
-//			System.out
-//					.println("-----------------------------------------------------------------------------------------------------");
-		}
-
-		return ssw;
-		// System.out.println(k + "  " + slKClustering);
+	public static String[] setToArray(Set<String> set) {
+		// 当参数数组的长度小于list的元素个数时，会自动扩充数组的长度以适应list的长度
+		String[] strArray = (String[]) set.toArray(new String[0]);
+		return strArray;
 	}
 	
+	public static double getSSWForString(Set<Set<String>> slKClustering, HashMap<UnorderedPair<String>, Double> similarityMap) {
+		double maxdistanceAll = 0;
+		int numSingleObject = 0;
+		for (Set<String> set : slKClustering) {
+			int setSize = set.size();
+			double maxdistance = 0;
+			double distanceIJ;
+			String[] arrayStrings = setToArray(set);
+			if (setSize > 1) {
+				// 得到所有的pair 的distance， 放到pairscore 的list 里面
+				for (int i = 0; i < arrayStrings.length; i++) {
+					String sI = arrayStrings[i];
+					for (int j = i + 1; j < arrayStrings.length; j++) {
+						String sJ = arrayStrings[j];
+						double similarityValue = similarityMap.get(new UnorderedPair<String>(sI, sJ));
+						distanceIJ = 1 - similarityValue;
 
+						if (distanceIJ > maxdistance) {
+							maxdistance = distanceIJ;
+						}						
+					}
+				}
+			} else {
+				numSingleObject += 1/arrayStrings.length;
+			}
+			
+			if (maxdistance > maxdistanceAll)
+				maxdistanceAll = maxdistance;				
+		}		
+		return maxdistanceAll + numSingleObject;
+	}
+	
+	public static double getSSWForStringList(Set<Set<List<String>>> slKClustering, HashMap<UnorderedPair<List<String>>, Double> similarityMap) {
+		double maxdistanceAll = 0;
+		int numSingleObject = 0;
+		for (Set<List<String>> set : slKClustering) {
+			int setSize = set.size();
+			double maxdistance = 0;
+			double distanceIJ;
+			List<List<String>> list = new ArrayList<List<String>>(set);
+			if (setSize > 1) {
+				// 得到所有的pair 的distance， 放到pairscore 的list 里面
+				for (int i = 0; i < setSize; i++) {
+					List<String> sI = list.get(i);
+					for (int j = i + 1; j < setSize; j++) {
+						List<String> sJ = list.get(j);
+						double similarityValue = similarityMap.get(new UnorderedPair<List<String>>(sI, sJ));
+						distanceIJ = 1 - similarityValue;
+						if (distanceIJ > maxdistance) {
+							maxdistance = distanceIJ;
+						}
+					}
+				}
+			} else {
+				numSingleObject += 1/list.size();
+			}
+			
+			if (maxdistance > maxdistanceAll)
+				maxdistanceAll = maxdistance;				
+		}
+		return maxdistanceAll + numSingleObject;
+	}
+	
+	public double getSSBForString(Set<Set<String>> slKClustering, HashMap<UnorderedPair<String>, Double> similarityMap) {
+		double ssbTotal = 0;
+		Object[] objArray = slKClustering.toArray();
+		double ssbDistanceMin = 1000000.0;
+
+		int normalizationFactor = 0;
+		for (int i = 0; i < objArray.length; i++) {
+			Set<String> sI = (Set<String>) objArray[i];
+			String[] sIArray = setToArray(sI);
+			for (int j = i + 1; j < objArray.length; j++) {
+				Set<String> sJ = (Set<String>) objArray[j];
+				String[] sJArray = setToArray(sJ);
+				List<Double> distancePairList = new ArrayList<Double>();
+				for (int l = 0; l < sIArray.length; l++) {
+					String temp1String = sIArray[l];
+					for (int m = 0; m < sJArray.length; m++) {
+						String temp2String = sJArray[m];
+						
+						double similarityValue = similarityMap.get(new UnorderedPair<String>(temp1String, temp2String));
+						double distanceIJ = 1 - similarityValue;
+
+						distancePairList.add(distanceIJ);
+					}
+				}
+				Collections.sort(distancePairList);
+				Object[] distancePairArray = distancePairList.toArray();
+				Double ssbDistance = (Double) distancePairArray[0];
+				int sI_size = sI.size();
+				int sJ_size = sJ.size();
+				if (sI_size > 1 || sJ_size > 1) {
+					ssbTotal += ssbDistance;
+					normalizationFactor += 1;
+				}
+			}
+		}
+		return ssbTotal;
+	}
+	
+	public double getSSBForStringList(Set<Set<List<String>>> slKClustering, HashMap<UnorderedPair<List<String>>, Double> similarityMap) {
+		List<Double> ssb = new ArrayList<Double>();
+			double ssbTotal = 0;
+			Object[] objArray = slKClustering.toArray();
+			double ssbDistanceMin = 1000000.0;
+
+			int normalizationFactor = 0;
+			for (int i = 0; i < objArray.length; i++) {
+				Set<List<String>> sI = (Set<List<String>>) objArray[i];
+				List<List<String>> sIArray = new ArrayList<List<String>>(sI);
+				for (int j = i + 1; j < objArray.length; j++) {
+					Set<List<String>> sJ = (Set<List<String>>) objArray[j];
+					List<List<String>> sJArray = new ArrayList<List<String>>(sJ);
+					List<Double> distancePairList = new ArrayList<Double>();
+					for (int l = 0; l < sIArray.size(); l++) {
+						List<String> temp1String = sIArray.get(l);
+						for (int m = 0; m < sJArray.size(); m++) {
+							List<String> temp2String = sJArray.get(m);
+							
+							double similarityValue = similarityMap.get(new UnorderedPair<List<String>>(temp1String, temp2String));
+							double distanceIJ = 1 - similarityValue;
+
+							distancePairList.add(distanceIJ);
+						}
+					}
+					//ascending order
+					Collections.sort(distancePairList);
+					Object[] distancePairArray = distancePairList.toArray();
+					Double ssbDistance = (Double) distancePairArray[0];
+					int sI_size = sI.size();
+					int sJ_size = sJ.size();
+					if (sI_size > 1 || sJ_size > 1) {
+						ssbTotal += ssbDistance;
+						normalizationFactor += 1;
+					}
+				}
+
+			}
+		return ssbTotal;
+	}
 	
 	
 	
